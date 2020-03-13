eed18539f4ea3181b2dd63ed524020304843ffc8
donghunee <ehdgns1766@naver.com>
2020-03-13 15:06:30 +0900

    Fsir

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.io.on("connection", socket => {
     let userWrap = {};
     userWrap.userList = socket.adapter.rooms[roomName].userList;
     userWrap.question = "랜덤 질문";
-    app.io.in(roomName.toString()).emit("questionOK", userWrap);
+    app.io.in(roomName.toString()).emit("gameState", userWrap);
   });
 
   socket.on("disconnect", () => {
